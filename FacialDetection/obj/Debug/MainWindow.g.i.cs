08aab66ddec4eacc5e7e5ce35888d757bd199f02
jaycdave88 <jaycdave@gmail.com>
2016-02-29 12:34:22 -0600

    added UI

--- FacialDetection/obj/Debug/MainWindow.g.i.cs
+++ FacialDetection/obj/Debug/MainWindow.g.i.cs
@@ -40,6 +40,30 @@ namespace FacialDetection {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 12 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.TabControl MyTabController;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 25 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Label lbStat;
+        
+        #line default
+        #line hidden
+        
+        
+        #line 33 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.TextBlock txtBStat;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +92,18 @@ namespace FacialDetection {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.MyTabController = ((System.Windows.Controls.TabControl)(target));
+            return;
+            case 2:
+            this.lbStat = ((System.Windows.Controls.Label)(target));
+            return;
+            case 3:
+            this.txtBStat = ((System.Windows.Controls.TextBlock)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
